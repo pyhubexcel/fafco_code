c138db188636aa7ef1121117bcfb3a0db6bb3198
aayush <aayush.excel2011@gmail.com>
2024-04-24 15:23:00 +0530

    created forget password and confirm password functionality and changes some other APIs also

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from .views import (
     RegisterAPI, LoginAPI, LogoutAPI, UpdatePasswordAPIView,
     PasswordChangeAPI, ProfileAPI, ProfileDetailAPI,
-    AutocompleteAPIView, SingleAddressValidationAPIView
+    AutocompleteAPIView, SingleAddressValidationAPIView,
+    PasswordResetAPIView, PasswordResetConfirmAPIView
 )
 
 urlpatterns = [
@@ -19,4 +22,11 @@ urlpatterns = [
     path('autocomplete/', AutocompleteAPIView.as_view(), name='autocomplete'),
     path('validation/', SingleAddressValidationAPIView.as_view(),
          name='validation'),
+    path('forgot-password/', PasswordResetAPIView.as_view(),
+         name='forgot_password'),
+    path('reset/confirm/<uidb64>/<token>/',
+         PasswordResetConfirmAPIView.as_view(), name='password_reset_confirm'),
+    path('reset-password/complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
